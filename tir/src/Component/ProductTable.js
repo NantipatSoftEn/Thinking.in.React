08b38355f3c6b83028dc3e60b53abb4dd28b1f67
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-09 12:20:00 +0700

    add producttable
    This reverts commit 9e6f04122073ce2ead23d5bb84fd5126963ae98c.

--- tir/src/Component/ProductTable.js
+++ tir/src/Component/ProductTable.js
@@ -6,25 +6,35 @@ import ProductRow from './ProductRow';
 
 class ProductTable extends Component {
   render() {
+
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
     const rows = [];
     let lastCategory = null;
 
     this.props.products.forEach((product) => {
-      if (product.category !== lastCategory) {
-        rows.push(
-          <ProductCategoryRow
-            category={product.category}
-            key={product.category} />
-        );
-      }
-      rows.push(
-        <ProductRow
-          product={product}
-          key={product.name} />
-      );
-      lastCategory = product.category;
-    });
-
+          if (product.name.indexOf(filterText) === -1) {
+            return;
+          }
+          if (inStockOnly && !product.stocked) {
+            return;
+          }
+          if (product.category !== lastCategory) {
+            rows.push(
+              <ProductCategoryRow
+                category={product.category}
+                key={product.category} />
+            );
+          }
+          rows.push(
+            <ProductRow
+              product={product}
+              key={product.name}
+            />
+          );
+          lastCategory = product.category;
+        });
     return (
       <table>
         <thead>
